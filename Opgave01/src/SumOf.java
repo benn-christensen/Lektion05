2c11a84e52cfde575d0d850e4c5190feab175e72
Benn Christensen <bnch@eaaa.dk>
2023-09-07 13:35:47 +0200

    spelling errors

--- Opgave01/src/SumOf.java
+++ Opgave01/src/SumOf.java
@@ -1,4 +1,10 @@
+
 public class SumOf {
+    /**
+     *
+     * @param a
+     * @return sum of even numbers
+     */
     public static int evenNumbers() {
         //Returner summen af alle lige tal fra 2 - 100,
         //begge inklusiv.
@@ -7,7 +13,7 @@ public class SumOf {
 
     public static int qubicNumbers() {
         //Returner summen af alle kvadrattal der er mindre en 100
-        //Et kvaddrattal er kvaddratet af et heltal, de første er 1, 4, 9,...
+        //Et kvadrattal er kvadratet af et heltal, de første er 1, 4, 9,...
         return 0;
     }
 }
